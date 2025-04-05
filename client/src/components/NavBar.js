5048ddecd237784cca7ebaae1b0155dfcc8cab43
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-04-05 14:35:32 -0500

    edit navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className='menu'>
-        <ul className='nav-sections'>
+        <nav className='navbar'>
+        <ul className='sections'>
             <li>
                 <Link to="/">Home</Link>
             </li>
             <li>
-                <Link to="/profile">My Profile</Link>
-            </li>
-            <li>
-                <Link to="/plants">Plants</Link>
-            </li>
-            <li>
                 <Link to="/recommend">Recommendations</Link>
+            </li>
+            <li>
+                <Link to="/plants">My Plants</Link>
+            </li>
+            <li>
+                <Link to="/profile">My Profile</Link>
             </li>
             <li>
                 <Link to="/signin">Sign In</Link>
