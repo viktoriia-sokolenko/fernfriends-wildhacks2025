ebca9571a40a801437bd801a7dd9482cda8e1159
Mingyuan Wang <mingyuanwang1216@gmail.com>
2025-04-05 15:14:58 -0500

    working form demo

--- client/src/pages/Recommendations.js
+++ client/src/pages/Recommendations.js
@@ -1,5 +1,44 @@
-const Recommendations = () => {
-    return <h1>Recommendations</h1>;
-  };
-  
-  export default Recommendations;
+import { useState } from 'react';
+
+function Recommendations() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(formData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="username" 
+        value={formData.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={formData.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
+}
+
+export default Recommendations;
